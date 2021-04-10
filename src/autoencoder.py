da89b1929a4182ebc0bf023189740df1c979552f
charleslin4 <charleslin4@gmail.com>
2021-04-10 16:31:50 -0700

    init

---
+++ src/autoencoder.py
@@ -0,0 +1,150 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from pytorch3d.loss import chamfer
+from general_utils import plot_3d_point_cloud
+import wandb
+import matplotlib.pyplot as plt
+
+class Encoder(nn.Module):
+
+    def __init__(self, in_channels, out_channels, points_per_cloud):
+        super(Encoder, self).__init__()
+        
+        self.conv1_bn = nn.Sequential(
+            nn.Conv1d(in_channels=in_channels, out_channels=64, kernel_size=1, stride=1),
+            nn.BatchNorm1d(64),
+            nn.ReLU()
+        )
+
+        self.conv2_bn = nn.Sequential(
+            nn.Conv1d(in_channels=64, out_channels=128, kernel_size=1, stride=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU()
+        )
+
+        self.conv3_bn = nn.Sequential(
+            nn.Conv1d(in_channels=128, out_channels=128, kernel_size=1, stride=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU()
+        )
+
+        self.conv4_bn = nn.Sequential(
+            nn.Conv1d(in_channels=128, out_channels=256, kernel_size=1, stride=1),
+            nn.BatchNorm1d(256),
+            nn.ReLU()
+        )
+
+        self.conv5_bn = nn.Sequential(
+            nn.Conv1d(in_channels=256, out_channels=out_channels, kernel_size=1, stride=1),
+            nn.BatchNorm1d(out_channels),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        out = self.conv1_bn(x)
+        out = self.conv2_bn(out)
+        out = self.conv3_bn(out)
+        out = self.conv4_bn(out)
+        out = self.conv5_bn(out)
+        out, _ = torch.max(out, 2)
+
+        return out
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+        super(Decoder, self).__init__()
+
+        self.fc1_bn = nn.Sequential(
+            nn.Linear(in_channels, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU()
+        )
+        self.fc2_bn = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU()
+        )
+        self.fc3 = nn.Sequential(
+            nn.Linear(128, out_channels)
+        )
+
+    def forward(self, x):
+        out = self.fc1_bn(x)
+        out = self.fc2_bn(out)
+        out = self.fc3(out)
+        
+        return out
+
+
+class AutoEncoder(pl.LightningModule):
+
+    def __init__(self, points_per_cloud, in_channels, bneck_size, loss='chamfer'):
+        super().__init__()
+
+        self.points_per_cloud = points_per_cloud
+        self.in_channels = in_channels
+        self.bneck_size = bneck_size
+        self.loss = loss.lower()
+
+        self.encoder = Encoder(in_channels, bneck_size, points_per_cloud)
+        self.decoder = Decoder(bneck_size, in_channels*points_per_cloud)
+
+    def forward(self, x):
+        embedding = self.encoder(x)
+        return embedding
+
+    def training_step(self, batch, batch_idx):
+        # training_step defined the train loop. It is independent of forward
+        x = batch
+        x = x.permute(0, 2, 1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z).view(-1, self.in_channels, self.points_per_cloud)
+
+        if self.loss == 'chamfer':
+            loss = chamfer.chamfer_distance(x_hat, x)[0]
+        elif loss == 'emd':
+            # TODO
+            print("Not yet implemented.")
+            import pdb; pdb.set_trace()
+        else:
+            print("Please choose either 'chamfer' or 'emd' for the loss.")
+            import pdb; pdb.set_trace()
+
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x = batch
+        x = x.permute(0, 2, 1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z).view(-1, self.in_channels, self.points_per_cloud)
+
+        if self.loss == 'chamfer':
+            loss = chamfer.chamfer_distance(x_hat, x)[0]
+        elif loss == 'emd':
+            # TODO
+            print("Not yet implemented.")
+            import pdb; pdb.set_trace()
+        else:
+            print("Please choose either 'chamfer' or 'emd' for the loss.")
+            import pdb; pdb.set_trace()
+
+        self.log('val_loss', loss)
+
+        if batch_idx % 10 == 0:
+            eval = x_hat[0].cpu().numpy()
+            fig = plot_3d_point_cloud(eval[0, :], 
+                                    eval[1, :], 
+                                    eval[2, :], in_u_sphere=True)
+            wandb.log({'eval': wandb.Image(fig)})
+            plt.close()
+
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=5e-4, weight_decay=0.001)
+        return optimizer
+
---
+++ src/train.py
@@ -0,0 +1,27 @@
+import os
+
+import torch
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+
+from torch.utils.data import DataLoader, random_split
+from in_out import snc_category_to_synth_id, create_dir, PointCloudDataSet, load_all_point_clouds_under_folder
+from autoencoder import AutoEncoder
+
+
+if __name__ == "__main__":
+    top_in_dir = './data/shape_net_core_uniform_samples_2048/'
+    class_name = 'chair'
+    syn_id = snc_category_to_synth_id()[class_name]
+    class_dir = os.path.join(top_in_dir, syn_id)
+    dataset = load_all_point_clouds_under_folder(class_dir, n_threads=8, file_ending='.ply', verbose=True)
+
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+    # TODO use config parameters
+    ae = AutoEncoder(2048, 3, 128)
+    wandb_logger = WandbLogger(name='test', project='3d-recon')
+    trainer = pl.Trainer(gpus=1, logger=wandb_logger)
+    trainer.fit(ae, DataLoader(train_dataset, batch_size=50, num_workers=2), DataLoader(val_dataset, batch_size=50, num_workers=2))
